b7b2a4d6aae284604f561354ee7c57154e1a6e4f
NikitaMoskvitin <129001078+NikitaMoskvitin@users.noreply.github.com>
2023-04-27 23:53:05 +0300

    Объект бинарное дерево, реализованный на классах

--- Tree_class.py
+++ Tree_class.py
@@ -37,6 +37,23 @@ class Tree:
                 Node = Node.right
                 Tree.__insert(self, Node, a)
 
+    def __insert_fake(self, Node, a):
+
+        '''ВНУТРЕННЯЯ ФУНКЦИЯ, вставляет звено в дерево, но наоборот'''
+
+        if a > Node.value:
+            if Node.left == None:
+                Node.left = Zveno(a)
+            else:
+                Node = Node.left
+                Tree.__insert_fake(self, Node, a)
+        else:
+            if Node.right == None:
+                Node.right = Zveno(a)
+            else:
+                Node = Node.right
+                Tree.__insert_fake(self, Node, a)
+
     def __find_full(self, a):
 
         '''ВНУТРЕННЯЯ ФУНКЦИЯ, ищет нужный элемент в дереве, начиная с корня'''
@@ -59,6 +76,25 @@ class Tree:
             Node = Node.left
             Node, back = Tree.__find(self, Node, back, a)
             return Node, back
+
+    def __find_parents(self, Node, back, a, c):
+
+        '''ВНУТРЕННЯЯ ФУНКЦИЯ, ищет нужный элемент в дереве'''
+
+        if Node.value == a:
+            return Node, back, c
+        elif Node.value < a:
+            back = Node
+            Node = Node.right
+            c.append(back.value)
+            Node, back, c = Tree.__find_parents(self, Node, back, a, c)
+            return Node, back, c
+        else:
+            back = Node
+            Node = Node.left
+            c.append(back.value)
+            Node, back, c = Tree.__find_parents(self, Node, back, a, c)
+            return Node, back, c
 
     def _create_tree(self):
 
@@ -255,6 +291,28 @@ class Tree:
         for i in range(len(b)):
             print(b[i], end=' ')
 
+    def __round_width_help(self, Node):
+
+        '''ВНУТРЕННЯЯ ФУНКЦИЯ, производит обход в ширину без печати, начиная с определного узла'''
+
+        a = []
+        b = []
+        a.append(Node.value)
+        b.append(Node.value)
+        while len(a) != 0:
+            Node, back = Tree.__find_full(self, a[0])
+            if b.count(Node.value) > 1:
+                for i in range(b.count(Node.value) - 1):
+                    Node = Node.left
+            if Node.left != None:
+                a.append(Node.left.value)
+                b.append(Node.left.value)
+            if Node.right != None:
+                a.append(Node.right.value)
+                b.append(Node.right.value)
+            a.pop(0)
+        return b
+
     def _delete_node(self, a):
 
         '''Эта функция удаляет звено'''
@@ -303,9 +361,46 @@ class Tree:
             else:
                 main_back.right = None
 
+    def _tree_sum(self):
+
+        '''Эта функция переделывает деерево в дерево сумм'''
+
+        a = Tree.__round_width_help(self, self.root)
+        c = []
+        for i in range(len(a)):
+            Node, back = Tree.__find_full(self, a.pop(0))
+            b = Tree.__round_width_help(self, Node)
+            s = 0
+            for j in range(len(b)):
+                s = s + b[j]
+            if s - b[0] != 0:
+                c.append(s - b[0])
+        self.root = None
+        self.root = Zveno(c[0])
+        for i in range(len(c) - 1):
+            Tree._insert_full(self, c[i + 1])
+
+    def _tree_mirror(self):
+
+        '''Эта функуия отражает дерево'''
+
+        a = Tree.__round_width_help(self, self.root)
+        self.root = None
+        self.root = Zveno(a[0])
+        for i in range(len(a) - 1):
+            Tree.__insert_fake(self, self.root, a[i + 1])
+
+    def _parents(self, a):
+
+        '''Эта функция выдает всех предков данного узла'''
+
+        c = []
+        Node, back, c = Tree.__find_parents(self, self.root, None, a, c)
+        return c
+
 t = Tree()
 while True:
-    print('1 - Cоздание дерева(числа вводятся через пробел) \n2 - Проверка дерева на пустоту \n3 - Удаляет дерво \n4 - Добавляет новое звено \n5 - Удаляет поддерево \n6 - Показывает максимальную глубину \n7 - Печатает дерево в прямом порядке \n8 - Печатает дерево в обратном порядке \n9 - Печать дерева в симметричном порядке \n10 - Проверка на сбалансированность \n11 - Удаление звена \n12 - Печать дерева обходом в ширину \n13 - Показывает минимальную глубину')
+    print('1 - Cоздание дерева(числа вводятся через пробел) \n2 - Проверка дерева на пустоту \n3 - Удаляет дерво \n4 - Добавляет новое звено \n5 - Удаляет поддерево \n6 - Показывает максимальную глубину \n7 - Печатает дерево в прямом порядке \n8 - Печатает дерево в обратном порядке \n9 - Печать дерева в симметричном порядке \n10 - Проверка на сбалансированность \n11 - Удаление звена \n12 - Печать дерева обходом в ширину \n13 - Показывает минимальную глубину \n14 - Печатает дерево сумм в симметричном порядке \n15 - Печать отзеркаленного дерева в симметричном порядке \n16 - Печатает всех предков введеного элемента')
     a = int(input())
     if a == 1:
         t._create_tree()
@@ -346,6 +441,23 @@ while True:
         print(end='\n')
     elif a == 13:
         print(t._find_depth_min_full())
+    elif a == 14:
+        b = t
+        b._tree_sum()
+        b._sym_print_full()
+        print(end='\n')
+    elif a == 15:
+        b = t
+        b._tree_mirror()
+        b._sym_print_full()
+        print(end='\n')
+    elif a == 16:
+        b = int(input())
+        c = t._parents(b)
+        for i in range(len(c)):
+            print(c[i], end=' ')
+        print(end='\n')
+
 
 
 
